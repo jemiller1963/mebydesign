3eea03cab3f791894d72617fbcba00d5b3bdfa8e
jemiller <jemiller1963@gmail.com>
2014-07-19 19:33:56 -0400

    Completed Event rspecs

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -55,8 +55,9 @@ subject { post :create , { 	event: {
 		end
 	end
 
-	describe "#index" do
+	describe "GET #index" do
 		it "populates an array of events" do
+			# USING FACTORIES
 			event = create(:event)
 			get :index
 			assigns(:events).should eq([event])
@@ -66,6 +67,77 @@ subject { post :create , { 	event: {
 			response.should render_template :index
 		end
 	end
+
+	describe "PUT #update" do
+		before :each do
+			@event = create(:event, event_name: 'Test Event 1',
+									description: 'Lorem',
+									      sdate: DateTime.new(2014, 10, 10),
+									      edate: DateTime.new(2014, 10, 12),
+									       link: 'www.example.com')
+		end
+		context "valid attributes" do
+			it "located the request @event" do
+				put :update, id: @event, event: attributes_for(:event)
+				assigns(:event).should eq(@event)
+			end
+			it "changes @event's attributes" do
+				put :update, id: @event,
+								event: attributes_for(:event, 
+									 event_name: 'Test Event 2',
+									description: 'Lorem ipsum dolor',
+									      sdate: DateTime.new(2014, 10, 10),
+									      edate: DateTime.new(2014, 10, 12),
+									       link: 'www.example.com/test')
+				@event.reload
+				@event.event_name.should eq("Test Event 2")
+				@event.description.should eq("Lorem ipsum dolor")
+				@event.sdate.should eq DateTime.new(2014, 10, 10)
+				@event.edate.should eq DateTime.new(2014, 10, 12)
+				@event.link.should eq("www.example.com/test")
+			end
+			it "redirects to updated event" do
+				put :update, id: @event, event: attributes_for(:event)
+				response.should redirect_to @event
+			end
+		end
+
+		context "invalid attributes" do
+			it "locates the requested @event" do
+				put :update, id: @event, event: attributes_for(:invalid_event)
+				assigns(:event).should eq(@event)
+			end
+			it "does not change @event's attributes" do
+				put :update, id: @event, event: attributes_for(	:event, 
+									 event_name: nil,
+									description: 'Lorem ipsum dolor',
+									      sdate: Date.new(2014, 10, 10),
+									      edate: Date.new(2014, 10, 12),
+									       link: 'www.example.com/test')
+				@event.reload
+				@event.event_name.should_not eq("Test Event 2")
+				@event.description.should_not eq("Lorem ipsum dolor")
+				@event.sdate.to_date.should_not eq Date.new(2014, 11, 10)
+				@event.edate.to_date.should_not eq Date.new(2014, 11, 12)
+				@event.link.should_not eq("www.example.com/test")
+			end
+			it "re-renders the edit method" do
+				put :update, id: @event, event: attributes_for(:invalid_event)
+				response.should render_template :edit
+			end
+		end
+	end
+
+
+
+
+
+
+
+
+
+
+
 
 
 end
--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -4,8 +4,8 @@ FactoryGirl.define do
 	factory :event do |e|
 		e.event_name     { Faker::Company.name} 
 		e.description    { Forgery(:lorem_ipsum).words(25) }
-		e.sdate          '10/10/2014'
-		e.edate          '10/12/2014'
+		e.sdate          Date.new(2014, 10, 10)
+		e.edate          Date.new(2014, 10, 12)
 		e.link           { Faker::Internet.url('example.com') }
 	end
 	factory :invalid_event, parent: :event do |e|
