9d9ad5b3438b8c4f57037eec4e18fb418c48849a
jemiller <jemiller1963@gmail.com>
2014-07-21 23:00:00 -0400

    -Updated event_controller and index to always show New Button

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,7 @@
 class EventsController < ApplicationController
   def index
-    # @events = Event.paginate(page: params[:page], per_page: 10).where("active = ?", true)
-    @events = Event.paginate(page: params[:page], per_page: 10)
+    @events = Event.paginate(page: params[:page], per_page: 10).where("active = ?", true)
+    # @events = Event.paginate(page: params[:page], per_page: 10)
   end
 
   def new
